1c64320931403ec6d4f46278733335b7545986c6
M. Policarpo <m.policarpo@coolblue.nl>
2016-07-12 13:23:37 +0200

    Fixed issues with the application arguments

--- DuprixUserProductAuthConverter/Program.cs
+++ DuprixUserProductAuthConverter/Program.cs
@@ -21,79 +21,86 @@ namespace UserGroupsCsvToJson
                 PrintParametersUsage("The parameter was not specified.");
                 return;
             }
-            if(args[1] == null)
-            {
-                PrintParametersUsage("The file path was not specified.");
-                return;
-            }
 
-            var filePath = args[1];
-            bool isFirstLineHeader = true;
             string parameter = args[0];
-
-            if(!File.Exists(filePath))
-                Console.WriteLine("The file path '{0}' does not exist.", filePath);
-            
-            var fileInfo = new FileInfo(filePath);
             var container = new UnityContainer();
-
             container.AddNewExtension<Bootstrapper>();
 
-            if(parameter == "-u")
+            if(parameter == "-purr")
             {
-                var userSettingsParser = container.Resolve<UserSettingsParser>();
-                var settingsRawDtos = userSettingsParser.Parse(filePath, isFirstLineHeader);
-
-                var settingsGenerator = new UserSettingsGenerator();
-                var userSettingsList = settingsGenerator.Generate(settingsRawDtos);
-
-                userSettingsParser.Export(userSettingsList, fileInfo.DirectoryName);
-                userSettingsParser.UploadSettings(userSettingsList);
-            }
-            else if(parameter == "-purr")
-            {                
                 var priceGroupStore = container.Resolve<PriceGroupStore>();
 
                 RepositoryResult<IEnumerable<PriceGroupDto>> priceGroupsRepositoryResult = priceGroupStore.GetAll();
                 if(priceGroupsRepositoryResult.Success)
                 {
-                    foreach (var priceGroup in priceGroupsRepositoryResult.Result)
+                    Console.WriteLine($"Retrieved {priceGroupsRepositoryResult.Result.Count()} price groups");
+
+                    foreach(var priceGroup in priceGroupsRepositoryResult.Result)
                     {
                         priceGroup.RoundingRules = true;
                         var updateResult = priceGroupStore.Update(priceGroup);
 
-                        if (updateResult.Success)
+                        if(updateResult.Success)
                             Console.WriteLine($"Updated price group {priceGroup.Id} - {priceGroup.Name}");
-                        else                        
-                            Console.WriteLine($"FAILED to updated price group {priceGroup.Id} - {priceGroup.Name}");   
+                        else
+                            Console.WriteLine($"FAILED to updated price group {priceGroup.Id} - {priceGroup.Name}");
                     }
                 }
             }
-            else if(parameter == "-p")
+            else
             {
-                var priceGroupsParser = container.Resolve<PriceGroupsParser>();
-                var priceGroupDtos = priceGroupsParser.Parse(filePath, isFirstLineHeader);
-
-                List<PriceGroupRawDto> tempPriceGroups = priceGroupDtos.ToList();
-
-                List<DuplicatePriceGroup> duplicates = new List<DuplicatePriceGroup>();
-                foreach (var pg in priceGroupDtos)
+                if (args[1] == null)
                 {
-                    foreach(var tpg in tempPriceGroups)
+                    PrintParametersUsage("The file path was not specified.");
+                    return;
+                }
+
+                var filePath = args[1];
+                bool isFirstLineHeader = true;
+
+                if(!File.Exists(filePath))
+                    Console.WriteLine("The file path '{0}' does not exist.", filePath);
+
+                var fileInfo = new FileInfo(filePath);
+
+                if(parameter == "-u")
+                {
+                    var userSettingsParser = container.Resolve<UserSettingsParser>();
+                    var settingsRawDtos = userSettingsParser.Parse(filePath, isFirstLineHeader);
+
+                    var settingsGenerator = new UserSettingsGenerator();
+                    var userSettingsList = settingsGenerator.Generate(settingsRawDtos);
+
+                    userSettingsParser.Export(userSettingsList, fileInfo.DirectoryName);
+                    userSettingsParser.UploadSettings(userSettingsList);
+                }
+                else if(parameter == "-p")
+                {
+                    var priceGroupsParser = container.Resolve<PriceGroupsParser>();
+                    var priceGroupDtos = priceGroupsParser.Parse(filePath, isFirstLineHeader);
+
+                    List<PriceGroupRawDto> tempPriceGroups = priceGroupDtos.ToList();
+
+                    List<DuplicatePriceGroup> duplicates = new List<DuplicatePriceGroup>();
+                    foreach(var pg in priceGroupDtos)
                     {
-                        if(pg.Name != tpg.Name && pg.ProductId == tpg.ProductId && pg.SubsidiaryId == tpg.SubsidiaryId)
+                        foreach(var tpg in tempPriceGroups)
                         {
-                            duplicates.Add( new DuplicatePriceGroup { Duplicate = tpg, FirstOccurrence = pg} );
+                            if(pg.Name != tpg.Name && pg.ProductId == tpg.ProductId &&
+                               pg.SubsidiaryId == tpg.SubsidiaryId)
+                            {
+                                duplicates.Add(new DuplicatePriceGroup {Duplicate = tpg, FirstOccurrence = pg});
+                            }
                         }
                     }
-                }                
 
-                var priceGroupGenerator = container.Resolve<PriceGroupGenerator>();
-                var priceGroups = priceGroupGenerator.Generate(priceGroupDtos);
+                    var priceGroupGenerator = container.Resolve<PriceGroupGenerator>();
+                    var priceGroups = priceGroupGenerator.Generate(priceGroupDtos);
 
-                priceGroupsParser.Export(priceGroups, fileInfo.DirectoryName);
-                priceGroupsParser.Export(duplicates, fileInfo.DirectoryName, "duplicates.json");
-                priceGroupsParser.Upload(priceGroups);
+                    priceGroupsParser.Export(priceGroups, fileInfo.DirectoryName);
+                    priceGroupsParser.Export(duplicates, fileInfo.DirectoryName, "duplicates.json");
+                    priceGroupsParser.Upload(priceGroups);
+                }
             }
         }
 
