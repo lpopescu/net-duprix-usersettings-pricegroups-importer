1757b6c4b0220abb1f9ee4d08d1136574315063e
policarpo <m.policarpo@coolblue.nl>
2016-09-30 09:19:43 +0200

    Adds GetProductTypesFor

--- DuprixUserProductAuthConverter/PriceGroups/ProductTypeStore.cs
+++ DuprixUserProductAuthConverter/PriceGroups/ProductTypeStore.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using net_product_webservice.Client.Repositories;
 using net_product_webservice.Dto.ProductHierarchy;
@@ -10,10 +12,12 @@ namespace UserGroupsCsvToJson
     public class ProductTypeStore
     {
         private readonly ProductTypeRepository _repository;
+        private readonly ProductRepository _productRepository;
 
-        public ProductTypeStore(ProductTypeRepository repository)
+        public ProductTypeStore(ProductTypeRepository repository, ProductRepository productRepository)
         {
             _repository = repository;
+            _productRepository = productRepository;
         }
 
         public string GetProductTypeName(int id)
@@ -28,6 +32,15 @@ namespace UserGroupsCsvToJson
         {
             RepositoryResult<ProductTypeDto> result = _repository.GetByIdAsync(id).Result;
             return result;
+        }
+
+        public IEnumerable<ProductTypeProductRelationDto> GetProductTypeId(IEnumerable<int> productIds)
+        {
+            var result = _productRepository.GetProductHierarchyByIdsAsync(productIds).Result;
+            if (result.Success)
+                return result.Result.Select( ph => new ProductTypeProductRelationDto { ProductId = ph.Product.Id, ProductType = ph.ProductType});
+
+            throw new Exception($"Failed to retrieve product hierarchy for {string.Join(",",productIds)}");
         }
     }
 }
